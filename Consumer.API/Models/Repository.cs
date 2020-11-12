f7e3f0cd63054012c8cbcbc1bb7cb5e8cd12d2b4
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-11-12 13:49:34 -0300

    refactor: added decorator to Name property

--- Consumer.API/Models/Repository.cs
+++ Consumer.API/Models/Repository.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace Consumer.Api.Models
 {
     public class Repository
     {
-        public string name { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
     }
 }
