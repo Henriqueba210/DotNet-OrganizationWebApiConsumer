8d40f5162379adc23b8821d2ddeb0a75ecc6075e
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-11-23 14:58:36 -0300

    refactor: made Program class static

--- Consumer.API/Program.cs
+++ Consumer.API/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Hosting;
 namespace Consumer.API
 {
     [ExcludeFromCodeCoverage]
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
